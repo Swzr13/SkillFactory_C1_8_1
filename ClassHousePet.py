38db6a3ad86b9ab8b05447e5382aca9a9abdf802
Swzr <Swetozar2005@mail.ru>
2021-03-04 12:40:41 +0300

    Initial repository

---
+++ ClassHousePet.py
@@ -0,0 +1,30 @@
+class Cat:
+    age = None
+    sex = None
+    def __init__(self, name, sex = '', age = 0):
+        self.name = name
+        self.sex = sex
+        self.age = age
+
+    def set_age(self, age):
+        if age > 0 and isinstance(age, int):
+            self.age = age
+
+    def set_sex(self, sex):
+        sex = sex.upper()
+        if sex == 'M' or sex == 'F':
+            self.sex = sex
+
+    def get_name(self):
+        return self.age
+
+    def get_sex(self):
+        return self.sex
+
+    def get_age(self):
+        return self.age
+
+    def print_cat(self):
+        print('Имя', self.name)
+        print('Пол', self.sex)
+        print('Возраст', self.age)
---
+++ HousePet.py
@@ -0,0 +1,12 @@
+from project.OOP.C1_8_1.ClassHousePet import Cat
+cat_1 = Cat(name='Барон>')
+cat_1.set_sex('m')
+cat_1.set_age(2)
+
+cat_2 = Cat(name='Murka')
+cat_2.set_sex('f')
+cat_2.set_age(2)
+
+cat_1.print_cat()
+print()
+cat_2.print_cat()
