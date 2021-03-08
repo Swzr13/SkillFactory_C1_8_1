5bd487bf0baa0c049fce4ba529e4b20309005c28
Swzr <Swetozar2005@mail.ru>
2021-03-08 14:08:26 +0300

    Edit code

--- ClassHousePet.py
+++ ClassHousePet.py
@@ -1,19 +1,17 @@
 class Cat:
-    age = None
-    sex = None
-    def __init__(self, name, sex = '', age = 0):
+    def __init__(self, name, sex, age):
         self.name = name
         self.sex = sex
         self.age = age
 
-    def set_age(self, age):
-        if age > 0 and isinstance(age, int):
-            self.age = age
-
-    def set_sex(self, sex):
-        sex = sex.upper()
-        if sex == 'M' or sex == 'F':
-            self.sex = sex
+    # def set_age(self, age):
+    #     if age > 0 and isinstance(age, int):
+    #         self.age = age
+    #
+    # def set_sex(self, sex):
+    #     sex = sex.upper()
+    #     if sex == 'M' or sex == 'F':
+    #         self.sex = sex
 
     def get_name(self):
         return self.age
--- HousePet.py
+++ HousePet.py
@@ -1,12 +1,6 @@
 from project.OOP.C1_8_1.ClassHousePet import Cat
-cat_1 = Cat(name='Барон>')
-cat_1.set_sex('m')
-cat_1.set_age(2)
+cats = [Cat(name='Барон', sex='мальчик', age='2 года'), \
+        Cat(name='Сэм', sex='мальчик', age='2 года')]
 
-cat_2 = Cat(name='Murka')
-cat_2.set_sex('f')
-cat_2.set_age(2)
-
-cat_1.print_cat()
-print()
-cat_2.print_cat()
+for pet in cats:
+    pet.print_cat()
